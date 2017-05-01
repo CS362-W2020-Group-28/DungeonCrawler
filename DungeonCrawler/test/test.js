d9abb5fc48dfcfb176581970d4da93f5dff0b95c
Joshua Jardenil <joshjardenil@live.com>
2017-05-01 12:03:58 -0700

    working on additionaltest cases

--- DungeonCrawler/test/test.js
+++ DungeonCrawler/test/test.js
@@ -9,14 +9,58 @@ describe('Initial', function(){
 });
 
 describe('Slime', function() {
-
-  it('should return slime', function() {
-    var testSlime = new Slime(12,12);
-    // assert.equal(testSlime, new Slime(12,12));
-  });
-
+  // Checks if slime is being read properly by test file
   it('should be of type slime', function() {
     var testSlime = new Slime(12,12);
     assert.equal(testSlime.type, "Slime");
   });
+});
+
+describe('Player', function() {
+  // Checks if slime is being read properly by test file
+  it('should be of type player', function() {
+    var player = new Player();
+    assert.equal(player.type, "Player");
+  });
+
+  it('should be facing down during initialization', function() {
+    var player = new Player();
+    assert.equal(player.facing, 0);
+  });
+
+  it('should have menuGoLeft working properly', function() {
+    var player = new Player();
+    player.menuGoRight();
+    player.menuGoRight();
+    player.menuGoLeft();
+    assert.equal(player.menuIndex, 1);
+  });
+
+  it('should have menuGoLeft hit 0 when at 0', function() {
+    var player = new Player();
+    player.menuGoLeft();
+    assert.equal(player.menuIndex, 0);
+  });
+
+  it('should have menuGoRight working properly', function() {
+    var player = new Player();
+    player.menuGoRight();
+    assert.equal(player.menuIndex, 1);
+  });
+
+	
+  it('should have setPosition working properly', function() {
+    var player = new Player();
+		var Scene = new Scene();
+
+		Scene.Start();
+		requestAnimationFrame(mainLoop);
+
+		var scenePosition = Scene.Camera.transform.position;
+
+		player.setPosition(16,16);
+
+    assert.equal(16, scenePosition.y);
+  });
+
 });
